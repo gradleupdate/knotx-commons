e31f60c18a0bf09b0195ababc3a602180cab2e65
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2019-06-25 10:50:52 +0200

    CR changes

--- src/main/java/io/knotx/commons/json/JsonObjectUtil.java
+++ src/main/java/io/knotx/commons/json/JsonObjectUtil.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright (C) 2019 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.knotx.commons.json;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import io.vertx.core.json.JsonObject;
@@ -11,7 +27,9 @@ public class JsonObjectUtil {
   }
 
   private static Function<JsonObject, String> toString(String key) {
-    return json -> json.getString(key);
+    return json -> Optional.ofNullable(json.getValue(key))
+        .map(Object::toString)
+        .orElse("");
   }
 
   public static String getString(String key, JsonObject parent) {
